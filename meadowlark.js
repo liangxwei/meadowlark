27694926116e239fcfbb9e1d088b239ac85eddbc
Oliver <ibabay1030@163.com>
2018-03-20 22:51:34 +0800

    init commit

---
+++ meadowlark.js
@@ -0,0 +1,34 @@
+var express = require('express');
+
+// create view engine
+var handlebars = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-c to terminate.');
+});
