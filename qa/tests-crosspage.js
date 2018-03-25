6a4dc62370a5dfa0ca0081930bfe4a6de5a9b492
Oliver <ibabay1030@163.com>
2018-03-25 22:50:42 +0800

    add grunt config.

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -11,7 +11,8 @@ suite('Cross-Page Tests', function(){
 
     test('visiting the "request group rate" page dirctly should result ' +
         'in an empty referrer field', function(done){
-        browser.visit('http://localhost:3000/tours/request-group-rate',
+            this.timeout(15000);
+            browser.visit('http://localhost:3000/tours/request-group-rate',
             function(){
             assert(browser.field('referrer').value === '');
             done();
@@ -20,6 +21,7 @@ suite('Cross-Page Tests', function(){
 
     test('requesting a group rate quote from the hood river tour page' +
     'should populate the referrer field', function(done){
+        this.timeout(15000);
         var referrer = 'http://localhost:3000/tours/hood-river';
         browser.visit(referrer, function(){
             browser.clickLink('.requestGroupRate', function(){
